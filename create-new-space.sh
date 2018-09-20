70980a92b63132c62425cd5c21bec78aab17dc2a
Marius Bogoevici <159684+mbogoevici@users.noreply.github.com>
2018-09-20 11:46:34 -0400

    Reorganize address creation logic

--- create-new-space.sh
+++ create-new-space.sh
@@ -1,1 +1,1 @@
-ansible-playbook --extra-vars "partner=$1" --ssh-extra-args "-o IdentitiesOnly=yes" provisioning/playbook-new-address-space.yml
+ansible-playbook --extra-vars "partner=$1" --ssh-extra-args "-o IdentitiesOnly=yes" provisioning/playbook-new-address-space-$2.yml
